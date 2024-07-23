fefcd988e1907f17087245985810319983b2123f
root <root@Hiten>
2024-07-23 18:07:49 +0930

    Count even numbers

--- count.cpp
+++ count.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
 int count(int array[], int n){
-    
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++){
+        if (array[i] % 2 == 0){
+            count++;
+        }
+    }
+
+    return count;
 }
