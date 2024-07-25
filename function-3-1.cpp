70be08f2f38a3fa3954a384d4bf8e374ffd56843
root <root@Hiten>
2024-07-25 23:09:49 +0930

    Med

---
+++ function-3-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+bool is_fanarray(int array[], int n){
+
+    if (n < 1){
+        return false;
+    }
+
+    int mid = n / 2; // finding the middle value
+
+    for (int i = 0; i < (mid - 1); i++){
+
+        // ascending (left to right)
+        if (array[i] > array[i + 1]){
+            return false;
+        }
+
+        // descending (right to left)
+        if (array[n] < array[n - 1]){
+            return false;
+        }
+    }
+
+    return true;
+
+
+}
