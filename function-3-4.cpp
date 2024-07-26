aff81596326112171c80557333589d670101dba4
root <root@Hiten>
2024-07-26 16:23:49 +0930

    ALL

--- function-3-4.cpp
+++ function-3-4.cpp
@@ -6,14 +6,19 @@ void print_pass_fail(char grade){
     switch (grade){
         case 'A':
             cout << "PASS" << endl;
+            break;
         case 'B':
             cout << "PASS" << endl;
+            break;
         case 'C':
             cout << "PASS" << endl;
+            break;
         case 'D':
             cout << "FAIL" << endl;
+            break;
         case 'E':
             cout << "FAIL" << endl;
+            break;
     }
 
 }
