e7246620cf3154f7d784f66cf123b41e46f4f31a
root <root@Hiten>
2024-08-03 15:32:30 +0930

    my

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,39 +1,43 @@
 #include <iostream>
-
-
-#include <iostream>
 #include <cmath>
 
 bool is_palindrome(int array[], int length) {
+
     if (length < 1) {
         return false; // Return false if n is less than 1
-    }
+    }   
 
-    int mid_point = ceil(length/2.0);
+    int mid = length / 2;
+    int index = length - 1;
 
-    // Check for palindrome property
-    for (int i = 0; i < mid_point; i++) {
-        if (array[i] != array[length - 1 - i]) {
-            return false; // If not equal, not a palindrome
+    for (int i = 0; i < mid + 1; i++){
+
+        if (array[i] != array[index]){
+            return false;
         }
+
+        index--;
     }
-    return true;   
+
+    return true;
 }
 
 int sum_array_elements(int integers[], int length){
     int sum = 0;
 
     for(int i = 0; i < length; i++){
-        sum = sum + integers[i];
+        sum += integers[i];
     }
 
     return sum;
 }
 
 int sum_if_palindrome(int integers[], int length) {
+
     if (is_palindrome(integers, length)) {
         return sum_array_elements(integers, length);
-    } else {
+    } 
+    else {
         return -2; // Not a palindrome
     }
 }
--- main-2-3.cpp
+++ main-2-3.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+extern bool sum_if_palindrome(int array[], int length);
+
 int main(){
-    
+
+    int  array[] = {1, 2, 3, 3, 1};
+    int length = 5;
+
+    int sum = sum_if_palindrome(array, length);
+
+    std:: cout << sum << "\n";
 }
