e0d2fb4e93d1cc10e89393eb5a739a142128099b
root <root@Hiten>
2024-08-14 22:07:44 +0930

    Less Go

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -7,11 +7,5 @@ extern PersonList createPersonList(int n);
 extern PersonList deepCopyPersonList(PersonList pl);
 
 int main(){
-    PersonList array = createPersonList(5);
-    PersonList array1 = deepCopyPersonList(array);
-
-    cout << array1.numPeople << endl;
-    cout << array1.people[2].name << endl;
-
     return 0;
 }
