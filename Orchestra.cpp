a5bf0a88628c79fa0f4798b443ca62df842cf517
root <root@Hiten>
2024-08-21 13:48:43 +0930

    Done

---
+++ Orchestra.cpp
@@ -0,0 +1,47 @@
+#include "Orchestra.h"
+#include "Musician.h"
+#include <string.h>
+
+using namespace std;
+
+Orchestra::Orchestra() {
+    num_musicians = 0;
+    max_musicians = 0;
+    musicians = nullptr;
+}
+
+Orchestra::Orchestra(int size) {
+    max_musicians = size;
+    musicians = new Musician[size];
+}
+
+int Orchestra::get_current_number_of_members() { return num_musicians; }
+
+bool Orchestra::has_instrument(std::string instrument) { 
+
+    for (int i = 0; i < num_musicians; i++){
+        // checking if the given instrument matches
+        if (musicians[i].get_instrument() == instrument){
+            return true;            
+        }
+    }
+
+    return false;
+}
+
+Musician* Orchestra::get_members() { return musicians; }
+
+bool Orchestra::add_musician(Musician new_musician) { 
+    
+    if (num_musicians == max_musicians){
+        return false;
+    }
+
+    this->musicians[num_musicians] = new_musician;
+    num_musicians++;
+    return true;
+}
+
+Orchestra::~Orchestra() {
+    delete[] musicians;
+}
