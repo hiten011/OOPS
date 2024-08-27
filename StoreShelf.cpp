7aae2113cfb2f47a2bc183bb6a075d76994714d5
root <root@Hiten>
2024-08-27 16:51:18 +0930

    hi

--- StoreShelf.cpp
+++ StoreShelf.cpp
@@ -1,42 +1,60 @@
 #include "StoreShelf.h"
 
-StoreShelf:: StoreShelf(){
-    max_width=0;
-    current_num=0;
-    current_width=0;
-    shelf=nullptr;
-};
+StoreShelf::StoreShelf() {
+    max_width = 0; // the maximum length of shelf
+    num = 0;
+    music_box_array = nullptr; // all the music_boxes on the shelf
+}
 
-StoreShelf:: StoreShelf(int width){
-    max_width=width;
-    current_num=0;
-    current_width=0;
-    shelf= new MusicBox[width]; // assuming that min width of a musicbox can be 1cm, we allocate `width` amount of memory
-};
+StoreShelf::StoreShelf(int width) {
+    this->max_width = width; // the maximum length of shelf
+}
 
-int StoreShelf::get_num_music_boxes(){
-    return this->current_num;
-};
+int StoreShelf::get_width() { return this->max_width; }
 
-int StoreShelf::get_width(){
-    return this->max_width;
-};
+int StoreShelf::get_num_music_boxes() { return this->num; }
 
-MusicBox* StoreShelf:: get_contents(){
-    return this->shelf;
-};
+MusicBox* StoreShelf::get_contents() { return this->music_box_array; }
 
-bool StoreShelf:: add_music_box(MusicBox a_music_box){
+bool StoreShelf::add_music_box(MusicBox a_music_box) {
 
-    if ((this->current_width+a_music_box.get_width()) <= this->max_width ) {
-        shelf[current_num] = a_music_box;
-        current_num++;
-        current_width += a_music_box.get_width();
-        return true;}
+    int current_length = 0;
 
-    return false;
-};
+    // calculating the current width on shelf
+    for (int i = 0; i < num; i++){
+        current_length += music_box_array[i].get_width();
+    }
 
-StoreShelf:: ~StoreShelf(){
-    delete[] this->shelf;
-};
+    // checking if the length is full
+    if (current_length + a_music_box.get_width() <= this->max_width){
+
+
+        // adding the MusicBox on Shelf
+        num++; // incrementing the value
+        MusicBox *temp = new MusicBox[num]; // creating new array
+        
+        // copying thevalue of previous array to new one
+        for (int j = 0; j < num - 1; j++){
+            temp[j] = music_box_array[j];
+        }
+
+        //  adding the new value
+        temp[num - 1] = a_music_box;
+
+        // deleting the intial array
+        delete[] music_box_array;
+
+        // setting the pointer
+        music_box_array = temp;
+
+        
+        return true;
+    }
+    else{
+        return false;
+    }
+ }
+
+ StoreShelf::~StoreShelf() {
+    delete[] music_box_array;
+ }
--- StoreShelf.h
+++ StoreShelf.h
@@ -1,25 +1,39 @@
-#ifndef STORESHELF_H
-#define STORESHELF_H
+#ifndef StoreShelf_H
+#define StoreShelf_H
 
+#include <iostream>
 #include <string>
 #include "MusicBox.h"
+
 using namespace std;
 
-class StoreShelf {
-    private:
-        int max_width;
-        int current_num;
-        int current_width;
-        MusicBox* shelf;
 
+class StoreShelf{
+
+    private:
+        int max_width; // the maximum length of shelf
+        int num; // to store the number of Music Boxes on
+        MusicBox *music_box_array; // all the music_boxes on the shelf
+    
     public:
-        StoreShelf();
-        StoreShelf(int width);
-        int get_num_music_boxes(); 
-        int get_width();
-        MusicBox *get_contents();
-        bool add_music_box(MusicBox a_music_box);
-        ~StoreShelf();
+    StoreShelf();                     // default constructor
+    StoreShelf(int width);            // constructor for shelf with given width in centimetres
+
+    int get_width();                  // returns the width of the shelf in centimetres
+
+    // returns the number of Music boxes currently on the shelf
+    int get_num_music_boxes(); 
+
+    // returns a dynamic array of the music boxes currently on the shelf
+    MusicBox *get_contents();
+
+    // returns true and adds music box to shelf if there is sufficient space
+    // otherwise returns false
+    bool add_music_box(MusicBox a_music_box);
+    
+    ~StoreShelf();
+
+        
 };
 
 #endif
