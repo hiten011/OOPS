9f4bd6e4466cae707b951e6ef1e392c584c327b7
root <root@Hiten>
2024-09-05 18:02:02 +0930

    hi

---
+++ Tesla.cpp
@@ -0,0 +1,47 @@
+#include "Tesla.h"
+
+Tesla::Tesla() {};
+Tesla::Tesla(char model, int price): model(model), Car(price), batteryPercentage(0.0) {}
+
+int Tesla::get_model() { return this->model; }
+int Tesla::set_model(char model) { this->model = model; }
+
+int Tesla::get_batteryPercentage() { return this->batteryPercentage; }
+int Tesla::set_batteryPercentage(int batteryPercentage) { 
+
+    if (batteryPercentage < 0){
+        this->batteryPercentage = 0;
+    } 
+    else if(batteryPercentage > 100){
+        this->batteryPercentage = 100;
+    } 
+    else{
+        this->batteryPercentage = batteryPercentage;
+    }
+}
+void Tesla::chargeBattery(int mins) {
+
+    for (int i = 0; i < mins; i++){
+
+        if (batteryPercentage == 100){
+            break;
+        }
+
+        batteryPercentage += 0.5;
+    }
+}
+
+void Tesla::drive(int kms) {
+    for (int i = 0; i < kms; i++){
+
+        if (batteryPercentage < 1){
+            break;
+        } 
+
+        if (i % 5 == 0){
+            batteryPercentage -= 1;
+        }
+
+        emissions += 74;
+    }
+};
---
+++ Tesla.h
@@ -0,0 +1,31 @@
+#ifndef TESLA_H
+#define TESLA_H
+
+#include <iostream>
+#include "Car.h"
+
+using namespace std;
+
+class Tesla: public Car{
+    protected:
+        char model;
+        float batteryPercentage;
+
+
+    public:
+        Tesla();
+        Tesla(char model, int price);
+
+        int get_model();
+        int set_model(char model);
+
+        int get_batteryPercentage();
+        int set_batteryPercentage(int batteryPercentage);
+
+        void chargeBattery(int mins);
+        void drive(int kms);
+
+
+};
+
+#endif
